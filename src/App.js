3f955b964f92d0ea329eaba88e28c306a9558e0e
Moriah Humphries <moriah.humphries@gmail.com>
2020-08-15 18:16:21 -0500

    updated state

--- src/App.js
+++ src/App.js
@@ -2,17 +2,45 @@ import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <h1>Hello World, I am a basic React app.</h1>
-            </header>
-            <Person name="Moriah" age="32"/>
-            <Person name="Andrew" age="35"/>
-            <Person name="Kerrigan" age="9">My Hobby: Meowing at night.</Person>
-        </div>
-    );
+
+class App extends Component {
+    // State - available only in classes in extend component in some versions of React. If state is updated, React will re-render
+    state = {
+        persons: [
+            { name: "Moriah", age: 32},
+            { name: "Andrew", age: 35},
+            { name: "Kerrigan", age: 9}
+        ],
+
+    }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1>Hello World, I am a basic React app.</h1>
+                </header>
+                <button>Switch Name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>My Hobby: Meowing at night.</Person>
+            </div>
+        );
+    }
 }
+
+// function App() {
+//     return (
+//         <div className="App">
+//             <header className="App-header">
+//                 <h1>Hello World, I am a basic React app.</h1>
+//             </header>
+//             <button>Switch Name</button>
+//             <Person name="Moriah" age="32"/>
+//             <Person name="Andrew" age="35"/>
+//             <Person name="Kerrigan" age="9">My Hobby: Meowing at night.</Person>
+//         </div>
+//     );
+// }
+
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const person = (props) => { // props = object with all properties passed in
-    return <p>I am {props.name} and I am {props.age} years old!</p>
+    return (
+        <div>
+            <p>I am {props.name} and I am {props.age} years old!</p>
+            {/* displays content that is a child of the opening and closing tag of Component*/}
+            <p>{props.children}</p>
+        </div>
+    )
 };
 
 export default person;
