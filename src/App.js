baef9f9d9addf447d0da59c0dc244c7345e3f4d4
Moriah Humphries <moriah.humphries@gmail.com>
2020-08-15 18:47:31 -0500

    refacted to useState using Hooks

--- src/App.js
+++ src/App.js
@@ -1,48 +1,77 @@
-import React, {Component} from 'react';
+// import React, {Component} from 'react';
+
+import React, {useState} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
+// React Hooks syntax, React 16.8 or higher
 
-class App extends Component {
-    // State - available only in classes in extend component in some versions of React. If state is updated, React will re-render
-    state = {
+const App = props => {
+    // array destructuring
+    const [personsState, setPersonsState] = useState({ // useState returns 2 elements, current state/new state, and update function
         persons: [
             {name: "Moriah", age: 32},
             {name: "Andrew", age: 35},
             {name: "Kerrigan", age: 9}
         ],
-    }
+        otherState: 'Other value'
+    });
 
-    // Standard naming convention for handler, pass reference onClick on element
-    switchNameHandler = () => {
-        // console.log("Was clicked")
-        // DONT DO THIS this.state.persons[0].name = "Moriah Elise";
+   const switchNameHandler = () => {
+    // console.log("Was clicked")
+    // DONT DO THIS personsState.persons[0].name = "Moriah Elise";
 
-        // Will update only changed information
-        this.setState({
-            persons: [
-                {name: "Moriah Elise", age: 32},
-                {name: "Andrew John", age: 35},
-                {name: "Kerrigan", age: 9}
-            ]
-        }) // takes object as argument and updates
-    }
-
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <h1>Hello World, I am a basic React app.</h1>
-                </header>
-                <button onClick={this.switchNameHandler}>Switch Name</button>
-                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>My Hobby: Meowing at
-                    night.</Person>
-            </div>
-        );
-    }
+    // Will update only changed information
+    setPersonsState({
+        persons: [
+            {name: "Moriah Elise", age: 32},
+            {name: "Andrew John", age: 35},
+            {name: "Kerrigan", age: 9}
+        ]
+    }) // takes object as argument and updates
 }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Hello World, I am a basic React app.</h1>
+            </header>
+            <button onClick={switchNameHandler}>Switch Name</button>
+            <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+            <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
+            <Person name={personsState.persons[2].name} age={personsState.persons[2].age}>My Hobby: Meowing at
+                night.</Person>
+        </div>
+    );
+}
+
+
+export default App;
+
+// State - available only in classes in extend component in some versions of React. If state is updated, React will re-render
+// state = {
+//     persons: [
+//         {name: "Moriah", age: 32},
+//         {name: "Andrew", age: 35},
+//         {name: "Kerrigan", age: 9}
+//     ],
+// }
+//
+//
+// // Standard naming convention for handler, pass reference onClick on element
+// switchNameHandler = () => {
+//     // console.log("Was clicked")
+//     // DONT DO THIS personsState.persons[0].name = "Moriah Elise";
+//
+//     // Will update only changed information
+//     this.setState({
+//         persons: [
+//             {name: "Moriah Elise", age: 32},
+//             {name: "Andrew John", age: 35},
+//             {name: "Kerrigan", age: 9}
+//         ]
+//     }) // takes object as argument and updates
+// }
 
 // function App() {
 //     return (
@@ -59,4 +88,4 @@ class App extends Component {
 // }
 
 
-export default App;
+
