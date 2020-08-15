e29ca28558e9cf4048475939d2ff5aaf8ac919d1
Moriah Humphries <moriah.humphries@gmail.com>
2020-08-15 17:38:14 -0500

    added person component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <header className="App-header">
         <h1>Hello World, I am a basic React app.</h1>
       </header>
+      <Person />
     </div>
   );
 }
