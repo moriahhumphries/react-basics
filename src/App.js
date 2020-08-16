30bce25e2ce7e0d3c668720a7e4c27c7b1061fd4
Moriah Humphries <moriah.humphries@gmail.com>
2020-08-15 20:53:44 -0500

    Added two way binding

--- src/App.js
+++ src/App.js
@@ -1,49 +1,72 @@
-// import React, {Component} from 'react';
+import React, {Component} from 'react';
 
-import React, {useState} from 'react';
+// import React, {useState} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-// React Hooks syntax, React 16.8 or higher
-
-const App = props => {
-    // array destructuring
-    const [personsState, setPersonsState] = useState({ // useState returns 2 elements, current state/new state, and update function
+class App extends Component {
+    // State - available only in classes in extend component in some versions of React. If state is updated, React will re-render
+    state = {
         persons: [
             {name: "Moriah", age: 32},
             {name: "Andrew", age: 35},
             {name: "Kerrigan", age: 9}
         ],
-        otherState: 'Other value'
-    });
+    }
 
-   const switchNameHandler = () => {
-    // console.log("Was clicked")
-    // DONT DO THIS personsState.persons[0].name = "Moriah Elise";
+    // Standard naming convention for handler, pass reference onClick on element
+    switchNameHandler = (newName) => {
+        // console.log("Was clicked")
+        // DONT DO THIS this.state.persons[0].name = "Moriah Elise";
 
-    // Will update only changed information
-    setPersonsState({
-        persons: [
-            {name: "Moriah Elise", age: 32},
-            {name: "Andrew John", age: 35},
+        // Will update only changed information
+        this.setState({
+            persons: [
+                {name: newName, age: 32},
+                {name: "Andrew John", age: 35},
+                {name: "Kerrigan", age: 9}
+            ]
+        }) // takes object as argument and updates
+    }
+
+    nameChangeHandler = (event) => {
+        this.setState({
+            persons: [
+            {name: "Moriah", age: 32},
+            {name: event.target.value, age: 35},
             {name: "Kerrigan", age: 9}
-        ]
-    }) // takes object as argument and updates
+            ]
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1>Hello World, I am a basic React app.</h1>
+                </header>
+                {/*// Can pass this way*/}
+                <button onClick={() => this.switchNameHandler("Moriah!!")}>Switch Name</button>
+                <Person
+                    name={this.state.persons[0].name}
+                    age={this.state.persons[0].age}/>
+                <Person
+                    name={this.state.persons[1].name}
+                    age={this.state.persons[1].age}
+                    changed={this.nameChangeHandler} />
+                <Person
+                    name={this.state.persons[2].name}
+                    age={this.state.persons[2].age}
+                    // Click to change name
+                    // click={this.switchNameHandler.bind(this, "Moriah")} >
+                    click={this.switchNameHandler.bind(this, "Moriah")} >
+                    My Hobby: Meowing at
+                    night.</Person>
+            </div>
+        );
+    }
 }
 
-    return (
-        <div className="App">
-            <header className="App-header">
-                <h1>Hello World, I am a basic React app.</h1>
-            </header>
-            <button onClick={switchNameHandler}>Switch Name</button>
-            <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
-            <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
-            <Person name={personsState.persons[2].name} age={personsState.persons[2].age}>My Hobby: Meowing at
-                night.</Person>
-        </div>
-    );
-}
 
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 const person = (props) => { // props = object with all properties passed in
     return (
+        // Can call methods as props to change parent component
         <div>
-            <p>I am {props.name} and I am {props.age} years old!</p>
+            <p onClick={props.click}>I am {props.name} and I am {props.age} years old!</p>
             {/* displays content that is a child of the opening and closing tag of Component*/}
             <p>{props.children}</p>
+
+            {/*Listening on Change - updates dynamically, value is prefilled*/}
+            <input type="text" onChange={props.changed} value={props.name}/>
         </div>
     )
 };
